64e9a5663168f939df677a832a822e527a9ccb96
Chris Roughsedge <chris.roughsedge@moneypenny.co.uk>
2025-11-26 11:49:33 +0000

    first commit

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-item clickable :href="isExternal ? props.link : undefined" :to="isExternal ? undefined : props.link">
+    <q-item-section v-if="props.icon" avatar>
+      <q-icon :name="props.icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ props.title }}</q-item-label>
+      <q-item-label caption>{{ props.caption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  caption: {
+    type: String,
+    default: '',
+  },
+
+  link: {
+    type: String,
+    default: '#',
+  },
+
+  icon: {
+    type: String,
+    default: '',
+  },
+})
+
+const isExternal = computed(() => {
+  return props.link.startsWith('http')
+})
+</script>
---
+++ src/pages/TimelinePage.vue
@@ -0,0 +1,9 @@
+<template>
+    <q-page class="flex flex-center">
+        <p>Timeline Page</p>
+    </q-page>
+</template>
+
+<script setup>
+//
+</script>
